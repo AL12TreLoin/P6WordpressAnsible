dfc9be03f4f9ad88d348978d53f8099dd917b0aa
AL12TreLoin <alain.monti54@laposte.net>
2021-10-24 17:16:00 +0200

    ajout du nom de serveur en automatique dans le fichier hosts

--- othertest.py
+++ othertest.py
@@ -1,14 +1,12 @@
 import os
+import getpass
+import subprocess
 
 print ("Avant de démarrer la configuration du serveur web, merci de répondre à ces questions: ")
-sitewp = str(input("Quel sera le nom du site Wordpress ? "))
-userwp = str(input("Quel sera le nom son administrateur ? "))
-passwdwp = str(input("Quel sera le mot de passe de son administrateur ? "))
+sitewp = str(input("Saisir le nom du site WordPress :"))
 
-#print ("Vérification des données: ")
-#print ("Le site est ",sitewp)
-#print ("L'utilisateur est ",userwp)
-#print ("Le mot de passe est ",passwdwp)
+userwp = str(input("Saisir le nom de l'administrateur de la base de données :"))
+passwdwp = getpass.getpass(prompt='Saisir un mot de passe pour cette base de données et valider par Entrée :')
 
 extras = 'extra1=' + sitewp + ' extra2=' + userwp + ' extra3=' + passwdwp
 
@@ -22,5 +20,9 @@ str5 = " -u user"
 str6 = " --become"
 str7 = " --ask-become-pass"
 str8 = " -vvv"
-
 os.system('ansible-playbook {}'.format(str1 + str2 + str3 + str4 + str5 + str6 + str7))
+
+cmd = "echo " + "192.168.106.2 " + sitewp + ".serverweb " + ">> /etc/hosts"
+subprocess.run(['sudo', 'bash', '-c', cmd])
+
+print ("\nl'adresse de votre site sera: ", sitewp + ".serverweb")
